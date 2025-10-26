308bcbb3c5c5fb36d3dfdbe148d42c71b0b1507c
SwizzCheese <zolud1st@gmail.com>
2025-10-25 23:05:11 -0500

    worked on CowAndHealthCounter

---
+++ scripts/health_and_cow_counter.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+var numOfCows = 0
+var numOfHearts = 3
+#sets the number of hearts and cows to numOfCows and numOfHearts
+@onready var health_label= $"CanvasLayer/VBoxContainer/Health Counter/health num text"
+@onready var cow_label= $"CanvasLayer/VBoxContainer/Cows Counter/cow collector num text"
+func _ready():
+	updateCowAndHealthLabel()
+
+func updateCowAndHealthLabel():
+	health_label.text = str(numOfHearts)  #converts numOfHearts to a string and gives to to health_label text
+	cow_label.text = str(numOfCows)       #converts numOfCows to a string and gives to to cow_label text
+	
+func increaseCows():    #increases cow count
+	numOfCows = numOfCows +1
+	
+func decreaseHearts():      #decreases number of hearts
+	numOfHearts = numOfHearts-1;
+	
+func resetHearts():     #resets numOfHearts (after dieing)
+	numOfHearts = 3
