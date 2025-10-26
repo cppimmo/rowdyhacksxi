32d7c7eb143236ae2d338de40211407741028d0f
gbplaunt22 <gavinplaunty7@gmail.com>
2025-10-25 23:47:30 -0400

    Add asteroid scene and supporting scripts
    Introduced asteroid.tscn with Area2D and CollisionShape2D nodes, each with their own scripts. Added playground.tscn for testing, and updated project.godot to set the main scene. These changes lay the groundwork for asteroid interactions in the game.

---
+++ area_2d.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+func _on_asteroid_enters_body(body: Node2D):
+	queue_free()
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
