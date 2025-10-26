3059401ab6f245af2779e50967a6ac1cbbfdb08f
Andrew Davis <ad532005@gmail.com>
2025-10-26 02:28:57 -0500

    Alien Cat Sprites

--- scenes/cow.gd
+++ scenes/cow.gd
@@ -2,7 +2,10 @@ extends Area2D
 
 func _ready():
 	self.body_entered.connect(_on_body_entered)
+@onready var health_and_cow_counter: Control = $"Health and Cow Counter"
 
-func _on_body_entered(body):
-	if body.is_in_group("player"):
+func _on_body_entered(body: Node2D) -> void:
+	var p: = body 
+	if p.is_in_group("destroyable"):
+		health_and_cow_counter.increaseCows()
 		queue_free()
