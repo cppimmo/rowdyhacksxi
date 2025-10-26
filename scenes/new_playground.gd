64e55323883e84ead441a2f4f3eae441dbb62272
SwizzCheese <zolud1st@gmail.com>
2025-10-26 01:18:49 -0500

    commit

---
+++ scenes/new_playground.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+#const Asteroid = preload("res://scenes/asteroid.tscn")
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	#var asteroidInstance = Asteroid.instantiate()
+	#add_child(asteroidInstance)
+	#asteroidInstance.startDespawnTimer()
+	
